bb2e688c9747bdec19fe92a70bc30cba0f202369
maclab <maclab@MacLab-MacBook.local>
2018-12-04 17:04:22 +0800

    deletion

--- Red-Black Tree/src/BlackRed_Tree.py
+++ Red-Black Tree/src/BlackRed_Tree.py
@@ -1,34 +1,37 @@
 
 class Node():
-    def __init__(self, value, parent, color, left=None, right=None):
+    def __init__(self, value, parent, color, left=None, right=None, is_nil=False):
         self.parent = parent
         self.left = left
         self.right = right
         self.value = value
         self.color = color
+        self.is_nil = is_nil
 
     def __iter__(self):
-        if self.left.color != "nil":
+        if self.left.is_nil is not True:
             yield from self.left.__iter__()
 
-        yield self.value
+        yield self, self.value
 
-        if self.right.color != "nil":
+        if self.right.is_nil is not True:
             yield from self.right.__iter__()
 
 
 class BRTree():
     def __init__(self):
-        self.NIL = Node(None, None, 'nil')
+        self.BLACK = 'black'
+        self.RED = 'red'
+        self.NIL = Node(None, None, self.BLACK, is_nil=True)
         self.root = self.NIL
 
     def __iter__(self):
-        if self.root.color == self.NIL:
+        if self.root == self.NIL:
             return list()
         yield from self.root.__iter__()
 
     def insert(self, value):
-        new_node = Node(value, None, 'red', left=self.NIL, right=self.NIL)
+        new_node = Node(value, None, self.RED, left=self.NIL, right=self.NIL)
         y = self.NIL
         x = self.root
         while x != self.NIL:
@@ -47,39 +50,67 @@ class BRTree():
         self._INSERT_FIXUP(new_node)
 
     def delete(self, value):
-        pass
+        z = self._SEARCH(value)
+        if z is None:
+            print('The value to be deleted not exists in the tree !')
+            return
+        y = z
+        y_origin_color = y.color
+
+        if z.left == self.NIL:
+            x = z.right
+            self._RB_TRANSPLANT(z, z.right)
+        elif z.right == self.NIL:
+            x = z.left
+            self._RB_TRANSPLANT(z, z.left)
+        else:
+            y = self._TREE_MINIMUM(z.right)
+            y_origin_color = y.color
+            x = y.right
+            if y.parent == z:
+                x.parent = y
+            else:
+                self._RB_TRANSPLANT(y, y.right)
+                y.right = z.right
+                y.right.parent = y
+            self._RB_TRANSPLANT(z, y)
+            y.left = z.left
+            y.left.parent = y
+            y.color = z.color
+        if y_origin_color == self.BLACK:
+            self._DELETE_FIXUP(x)
 
     def _INSERT_FIXUP(self, z):
-        while z.parent.color == "red":
+        while z.parent.color == self.RED:
             if z.parent == z.parent.parent.left:
                 y = z.parent.parent.right
-                if y.color == "red":
-                    z.parent.color = "black"
-                    y.color = "black"
-                    z.parent.parent.color = "red"
+                if y.color == self.RED:
+                    z.parent.color = self.BLACK
+                    y.color = self.BLACK
+                    z.parent.parent.color = self.RED
                     z = z.parent.parent
                 else:
                     if z == z.parent.right:
                         z = z.parent                      # case2
                         self._LEFT_ROTATE(z)              # case2 -> case3
-                    z.parent.color = "black"              # case3
-                    z.parent.parent.color = "red"         # case3
+                    z.parent.color = self.BLACK              # case3
+                    z.parent.parent.color = self.RED         # case3
                     self._RIGHT_ROTATE(z.parent.parent)   # case3
             elif z.parent == z.parent.parent.right:
-                y = z.parent.parent.right
-                if y.color == "red":
-                    z.parent.color = "black"
-                    y.color = "black"
-                    z.parent.parent.color = "red"
+                y = z.parent.parent.left
+                if y.color == self.RED:
+                    z.parent.color = self.BLACK
+                    y.color = self.BLACK
+                    z.parent.parent.color = self.RED
                     z = z.parent.parent
                 else:
-                    if z == z.parent.right:
+                    if z == z.parent.left:
                         z = z.parent                      # case2
-                        self._LEFT_ROTATE(z)              # case2 -> case3
-                    z.parent.color = "black"              # case3
-                    z.parent.parent.color = "red"         # case3
-                    self._RIGHT_ROTATE(z.parent.parent)   # case3
-        self.root.color = "black"
+                        self._RIGHT_ROTATE(z)              # case2 -> case3
+                    z.parent.color = self.BLACK             # case3
+                    z.parent.parent.color = self.RED         # case3
+                    self._LEFT_ROTATE(z.parent.parent)   # case3
+        self.root.color = self.BLACK
 
     def _LEFT_ROTATE(self, x):
         y = x.right
@@ -110,3 +141,72 @@ class BRTree():
             y.parent.right = x
         x.right = y
         y.parent = x
+
+    def _RB_TRANSPLANT(self, u, v):
+        if u.parent == self.NIL:
+            self.root = v
+        elif u == u.parent.left:
+            u.parent.left = v
+        else:
+            u.parent.right = v
+        v.parent = u.parent
+
+    def _SEARCH(self, value):
+        z = None
+        for node, val in self:
+            if val == value:
+                z = node
+        return z
+
+    def _TREE_MINIMUM(self, z):
+        while z.left != self.NIL:
+            z = z.left
+        return z, z.value
+
+    def _DELETE_FIXUP(self, x):
+        while x != self.root and x.color == self.BLACK:
+            if x == x.parent.left:
+                w = x.parent.right
+                if w.color == self.RED:
+                    w.color = self.BLACK                                   # case 1
+                    x.parent.color = self.RED                              # case 1
+                    self._LEFT_ROTATE(x.parent)                         # case 1
+                    w = x.parent.right                                  # case 1
+                else:
+                    if w.left.color == self.BLACK and w.right.color == self.BLACK:
+                        w.color = self.RED                                 # case 2
+                        x = x.parent                                    # case 2
+                    else:
+                        if w.right.color == self.BLACK:
+                            w.left.color = self.BLACK                      # case 3
+                            w.color = self.RED                             # case 3
+                            self._RIGHT_ROTATE(w)                       # case 3
+                            w = x.parent.right                          # case 3
+                        w.color = x.parent.color                        # case 4
+                        x.parent.color = self.BLACK                        # case 4
+                        w.right.color = self.BLACK                         # case 4
+                        self._LEFT_ROTATE(x.parent)                     # case 4
+                        x = self.root                                   # case 4
+            elif x == x.parent.right:
+                w = x.parent.left
+                if w.color == self.RED:
+                    w.color = self.BLACK                                   # case 1
+                    x.parent.color = self.RED                              # case 1
+                    self._RIGHT_ROTATE(x.parent)                         # case 1
+                    w = x.parent.left                                  # case 1
+                else:
+                    if w.left.color == self.BLACK and w.right.color == self.BLACK:
+                        w.color = self.RED                                 # case 2
+                        x = x.parent                                    # case 2
+                    else:
+                        if w.left.color == self.BLACK:
+                            w.right.color = self.BLACK                          # case 3
+                            w.color = self.RED                                 # case 3
+                            self._LEFT_ROTATE(w)                           # case 3
+                            w = x.parent.left                              # case 3
+                        w.color = x.parent.color                            # case 4
+                        x.parent.color = self.BLACK                            # case 4
+                        w.left.color = self.BLACK                             # case 4
+                        self._RIGHT_ROTATE(x.parent)                         # case 4
+                        x = self.root
+        x.color = self.BLACK
--- Red-Black Tree/src/Main.py
+++ Red-Black Tree/src/Main.py
@@ -1,6 +1,8 @@
 from BlackRed_Tree import BRTree
+import os
 
 input_path = "./input.txt"
+output_path = "./output.txt"
 
 
 def main():
@@ -11,15 +13,23 @@ def main():
             oper = f.readline().strip('\n')
             values = [int(v) for v in f.readline().split(' ')]
             if oper == '1':
+                oper_text = "Insert: " + " ,".join([str(val) for val in values])
                 for val in values:
                     brtree.insert(int(val))
             elif oper == '2':
+                oper_text = "Delete: " + " ,".join([str(val) for val in values])
                 for val in values:
                     brtree.delete(int(val))
-            
-            for node in brtree:
-                print(node)
-                
+            with open(output_path, 'a') as fout:
+                fout.write(oper_text + "\n")
+                for node, val in brtree:
+                    key = val
+                    parent = node.parent.value if node.parent.value is not None else " "
+                    color = node.color
+                    fout.write("key: {} parent: {} color: {} \n".format(key, parent, color))
+
 
 if __name__ == '__main__':
+    if os.path.exists(output_path):
+        os.remove(output_path)
     main()
